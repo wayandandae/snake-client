18ce60226ea8ebbd590cdddf6933f9220a486fd5
Jack Kang <jkang0724@hotmail.com>
2022-11-27 22:58:25 -0500

    Add connection success message and send client name to server

--- client.js
+++ client.js
@@ -10,6 +10,11 @@ const connect = () => {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("NAME: JHK");
+  });
+
   conn.on("data", data => {
     console.log(`Server: ${data}`);
   });

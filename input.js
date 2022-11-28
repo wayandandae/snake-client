22f57347ce26207864f585151db5db1c88d2cfdc
Jack Kang <jkang0724@hotmail.com>
2022-11-27 23:23:46 -0500

    Add and export input module

---
+++ input.js
@@ -0,0 +1,19 @@
+// check user key input and terminate if the input is ctrl+c
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const { connect } = require('./client');
-
-// check user key input and terminate if the input is ctrl+c
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-// setup interface to handle user input from stdin
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
 connect();
+
 setupInput();

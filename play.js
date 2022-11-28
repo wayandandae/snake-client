783a6cb2abd3613dea434f0337d20d6e6e9c9524
Jack Kang <jkang0724@hotmail.com>
2022-11-27 23:17:23 -0500

    Add user input checker to terminate the process

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const { connect } = require('./client');
 
+// check user key input and terminate if the input is ctrl+c
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();

faab84a6856553c9b9f1671d99e7bbae9e81c8c7
Jack Kang <jkang0724@hotmail.com>
2022-11-27 22:37:06 -0500

    Export and import using ES6 shorthand syntax

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = () => {
+  const conn = net.createConnection({
+    host: "172.25.223.217", // IP address here,
+    port: "50541" // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", data => {
+    console.log(`Server: ${data}`);
+  });
+
+  return conn;
+};
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "172.25.223.217", // IP address here,
-    port: "50541" // PORT number here,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", data => {
-    console.log(`Server: ${data}`);
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
